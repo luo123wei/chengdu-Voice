e006021de197eb2a74cc735207509436b1dd89ba
luo123wei <kylw02@126.com>
2026-07-25 15:06:06 +0800

    更新Clarity监控代码的Project ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "xxxxx");`,
+})(window, document, "clarity", "script", "xrth1n5csu");`,
           }}
         />
       </body>
